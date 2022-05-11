0e929bde20f1f1b82a4618ce4766b4f10a991efb
xingsf <33599027+xingsf@users.noreply.github.com>
2022-05-11 17:31:13 +0800

    Create get_dirs.py

---
+++ get_dirs.py
@@ -0,0 +1,18 @@
+import os,sys
+
+def explore(dir):
+    for root, dirs, files in os.walk(dir):
+        print('debug: ', root, dirs, files) # 这行用来调试，帮助理解代码    
+        for file in files:
+            path = os.path.join(root, file)
+            print(path)
+
+ 
+def main():
+    for path in sys.argv[1:]:           
+        if os.path.isdir(path):
+            explore(path)
+			
+			
+if __name__="__main__":
+    main()
